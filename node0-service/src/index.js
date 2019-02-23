1da59f613513338740addad87e103bdd5b646d7d
wangyuanhang <13588746259@163.com>
2019-02-23 14:30:55 +0800

    zuul网关fallback方法

---
+++ node0-service/src/index.js
@@ -0,0 +1,7 @@
+var http = require('http')
+var url = require('url')
+var path = require('path')
+
+var server = http.createServer(function (req, res) {
+    
+})
--- test/src/main/java/lianxi/cloud/test/Main.java
+++ test/src/main/java/lianxi/cloud/test/Main.java
@@ -1,29 +1,67 @@
 package lianxi.cloud.test;
 
-import lianxi.cloud.test.comm.BaseTest;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-
-import java.io.IOException;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 
 /**
- * @author: WangYuanHang
- * @Description:
- * @date: 2019/2/19 11:12
+ * @author wangyuanhang
  */
-public class Main {
-    public static void main(String[] args) {
-        BaseTest.execTest(() -> {
-            // 获取堆内存的初始值和最大值
-            // 物理内存的1/64
-            long l = Runtime.getRuntime().totalMemory();// 字节
-            // 物理内存的1/4
-            long m = Runtime.getRuntime().maxMemory();
-            // totalMemory:121MB
-            System.out.println("l=" + l / 1024 / 1024 + "MB");
-            // maxMemory:1787MB
-            System.out.println("Max=" + m / 1024 / 1024 + "MB");
-        }, 1);
+public class Main extends ChannelHandlerAdapter {
+
+    private final ByteBuf byteMsg;
+
+    public Main() {
+        byte[] req="我是请求数据llllll".getBytes();
+        byteMsg=Unpooled.buffer(req.length);
+        byteMsg.writeBytes(req);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(byteMsg);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf=(ByteBuf)msg;
+        byte[] req=new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body=new String(req,"UTF-8");
+        System.out.println("body:"+body);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        //释放资源
+        ctx.close();
+    }
+
+    public static void main(String[] args) throws Exception{
+        connect("127.0.0.1", 10001);
+    }
+    public static void connect(String addr,int port) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new Main());
+                        }
+                    });
+            ChannelFuture f = b.connect(addr, port);
+            f.channel().closeFuture().sync();//等待客户端关闭连接
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+            group.shutdownGracefully();
+        }
     }
 }
